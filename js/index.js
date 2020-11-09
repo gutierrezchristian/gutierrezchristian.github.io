b74c1034db98c4fd7ce81555c4b0a32316a9e466
Christian <christian@virtualvelociraptor.com>
2020-11-09 12:52:45 -0800

    adding init cont

---
+++ js/index.js
@@ -0,0 +1,17 @@
+var myLogo = document.getElementById('vv-logo');
+
+function hover(myLogo) {
+  myLogo.setAttribute('src','/images/Virtual_Velociraptor_Logo_Teal.png');
+}
+
+function unhover(myLogo) {
+  myLogo.setAttribute('src', '/images/Virtual_Velociraptor_Logo_Teal.png');
+}
+/*
+function hover(myLogo) {  myLogo.setAttribute('src','https://previews.dropbox.com/p/thumb/AAakhd0mcbhy1YJAtnxe4tnGu2bkdgsyyd1w21kj7doXa18laVVUYMcYnw0rjFqNOR21leB3LceZg8KRpQlhyTtocR3TXf92mntzi8brKDf2KcIOQS2C-3abdH-dPMjE50OB9ZiAOI6LZYGfI6xo6quabZw_gBwBjIHLkZmjruiZzgg6AySAOMSirBwn4Xd7JZTnOdRp8cRH0QfEpfynDYOGjRqmNQapN2cagfSroL5MLmqFCk_NCRoQBx887rcBbTVqSXz-CsFnv9rnDKw31oBCS6Ewqwuixi7M_k77g1ontLbXi1eshJV9bi8RNkIj8PejE6Ln6ra8477Ai381VsYb/p.png?fv_content=true&size_mode=5');
+}
+
+function unhover(myLogo) {
+  myLogo.setAttribute('src', 'https://previews.dropbox.com/p/thumb/AAbRMftbEX9ugf7JXKy53Jga7YZuIB4SX_fb-QfFNqP3NfcKVp8SOjWxubeJSHXyq9P_j9NWYZ9D9j7J0OC8yMZdxFaIs52VvkSWpAoU7hQ7VvgYvF_zqNike3CD03PNVZ8bhI9lX-5DFvltcxYTz965Uy_J9cnMHliy__Z7Vh1Lptvf_aYfxZIPua3KgzTUUypHXRMOGO5215oqAXtuix3RmjWY6GXODZViCxVrDIm2tIArp7S62y3WqXIqjg2NOM1WWElnjsMQ_hgm0h5ff5OuSO78Xgi63hVJHtnoTjSBbAD-bkI2v8E2fLQRczeZyTb2ZjHK3ex1WvenKXXXQqR_/p.png?fv_content=true&size_mode=5');
+}
+*/
